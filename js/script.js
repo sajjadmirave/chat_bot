5ee24ba9a93fd4f21383da12a8e38f2c7042ac39
sajjad <banditofsocorstars@gmail.com>
2022-02-16 08:45:55 -0800

    create the messages

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function send() {
+  let carl = document.getElementById("carl").value;
+  let user = document.getElementById("user").value;
+  let input = document.getElementById("input").value;
+
+  if (input === "hi") {
+    document.getElementById("carl").innerHTML = "hi";
+    
+  }
+}
